9a1cffc5342515c1e593b587c1425ae6fad32e93
sesath <sesathdecosta@gmail.com>
2021-04-02 12:48:00 +0530

    Create package structure

---
+++ src/test/java/com/training/deparmentservice/DeparmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.deparmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeparmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
